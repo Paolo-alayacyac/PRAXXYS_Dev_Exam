9f9918ed5ae884cf236d680d58aec9af80420299
Paolo Alayacyac <alayacyacpaolo1365@gmail.com>
2023-12-06 01:01:33 +0800

    update

--- src/Views/components/ProductBonchon.vue
+++ src/Views/components/ProductBonchon.vue
@@ -2,17 +2,14 @@
 	<a href="/summary">
 		<div class="pcontent ion-padding">
 			<div class="foods">
-				<img
-					:src="image"
-					alt="food"
-					class="foodimg" />
+				<img :src="image" alt="food" class="foodimg" />
 			</div>
 			<div class="foodproduct ">
 				<p class="title">{{ name }}</p>
 				<p class="title2 line-clamp-1">{{ description }}</p>
 				<div class="flex-between">
 					<ion-text class="pricetext"> P {{ price }} </ion-text>
-					<ion-text> Rate {{ rate}} </ion-text>
+					<ion-text> Rate {{ rate }} </ion-text>
 					<!-- <img
 						:src="rate"
 						alt="rating" /> -->
@@ -23,24 +20,28 @@
 </template>
 
 <script setup lang="ts">
-	const props = defineProps({
-		name: {
-			type: String,
-		},
-		description: {
-			type: String,
-			default: "",
-		},
-		price: {
-			type: Number,
-		},
-		image: {
-			type: String,
-            default: "/assets/product/food.png",
-		},
-        rate: {
-			type: String,
-            default: "/assets/product/Rating.svg",
-		},
-	});
+import {
+	IonText,
+} from "@ionic/vue"
+
+const props = defineProps({
+	name: {
+		type: String,
+	},
+	description: {
+		type: String,
+		default: "",
+	},
+	price: {
+		type: Number,
+	},
+	image: {
+		type: String,
+		default: "/assets/product/food.png",
+	},
+	rate: {
+		type: Number,
+		// default: "/assets/product/Rating.svg",
+	},
+});
 </script>
